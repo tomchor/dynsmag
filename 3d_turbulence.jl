77adeecde75ac08bd2b32772f83ac4f0b45cedbc
tomaschor <tomaschor@gmail.com>
2024-06-22 12:11:27 -0700

    more stuff

--- 3d_turbulence.jl
+++ 3d_turbulence.jl
@@ -25,11 +25,10 @@ AG = Oceananigans.Grids.AbstractGrid
 @inline ℱxy²ᵟ(i, j, k, grid, f, args...) = ℱy²ᵟ(i, j, k, grid, ℱx²ᵟ, f, args...)
 @inline ℱyz²ᵟ(i, j, k, grid, f, args...) = ℱz²ᵟ(i, j, k, grid, ℱy²ᵟ, f, args...)
 @inline ℱxz²ᵟ(i, j, k, grid, f, args...) = ℱz²ᵟ(i, j, k, grid, ℱz²ᵟ, f, args...)
-@inline ℱxyz²ᵟ(i, j, k, grid, f, args...) = ℱz²ᵟ(i, j, k, grid, ℱy²ᵟ, ℱx²ᵟ, args...)
+@inline ℱxyz²ᵟ(i, j, k, grid, f, args...) = ℱz²ᵟ(i, j, k, grid, ℱxy²ᵟ, f, args...)
 
 
 @inline fψ_plus_gφ²(i, j, k, grid, f, ψ, g, φ) = (f(i, j, k, grid, ψ) + g(i, j, k, grid, φ))^2
-
 function strain_rate_tensor_modulus_ccc(i, j, k, grid, u, v, w)
     Sˣˣ² = ∂xᶜᶜᶜ(i, j, k, grid, u)^2
     Sʸʸ² = ∂yᶜᶜᶜ(i, j, k, grid, v)^2
@@ -42,13 +41,28 @@ function strain_rate_tensor_modulus_ccc(i, j, k, grid, u, v, w)
     return √(Sˣˣ² + Sʸʸ² + Sᶻᶻ² + 2 * (Sˣʸ² + Sˣᶻ² + Sʸᶻ²))
 end
 
+@inline fψ̄_plus_gφ̄²(i, j, k, grid, f, ψ, g, φ) = (f(i, j, k, grid, ℱxyz²ᵟ, ψ) + g(i, j, k, grid, ℱxyz²ᵟ, φ))^2
+function filtered_strain_rate_tensor_modulus_ccc(i, j, k, grid, u, v, w)
+    Sˣˣ² = ∂xᶜᶜᶜ(i, j, k, grid, ℱxyz²ᵟ, u)^2
+    Sʸʸ² = ∂yᶜᶜᶜ(i, j, k, grid, ℱxyz²ᵟ, v)^2
+    Sᶻᶻ² = ∂zᶜᶜᶜ(i, j, k, grid, ℱxyz²ᵟ, w)^2
+
+    Sˣʸ² = ℑxyᶜᶜᵃ(i, j, k, grid, fψ_plus_gφ², ∂yᶠᶠᶜ, u, ∂xᶠᶠᶜ, v) / 4
+    Sˣᶻ² = ℑxzᶜᵃᶜ(i, j, k, grid, fψ_plus_gφ², ∂zᶠᶜᶠ, u, ∂xᶠᶜᶠ, w) / 4
+    Sʸᶻ² = ℑyzᵃᶜᶜ(i, j, k, grid, fψ_plus_gφ², ∂zᶜᶠᶠ, v, ∂yᶜᶠᶠ, w) / 4
+
+    return √(Sˣˣ² + Sʸʸ² + Sᶻᶻ² + 2 * (Sˣʸ² + Sˣᶻ² + Sʸᶻ²))
+end
+
 ϕ_times_fψ(i, j, k, grid, ϕ, f::Function, ψ) = ϕ
 function MᵢⱼMᵢⱼ_ccc(i, j, k, grid, u, v, w, p)
     S_abs = strain_rate_tensor_modulus_ccc(i, j, k, grid, u, v, w)
 
-    var"⟨|S|S₁₁⟩" = ℱxyz²ᵟ(i, j, k, grid, ϕ_times_ψ, S_abs, ∂xᶜᶜᶜ, u)
+    var"⟨|S|S₁₁⟩" = ℱxyz²ᵟ(i, j, k, grid, ϕ_times_fψ, S_abs, ∂xᶜᶜᶜ, u)
+    var"⟨|S|S₂₂⟩" = ℱxyz²ᵟ(i, j, k, grid, ϕ_times_fψ, S_abs, ∂yᶜᶜᶜ, v)
+    var"⟨|S|S₃₃⟩" = ℱxyz²ᵟ(i, j, k, grid, ϕ_times_fψ, S_abs, ∂zᶜᶜᶜ, w)
 
-    #m₁₁² = p.α^2 * p.β * S_abs * ∂xᶜᶜᶜ(i, j, k, grid, ℱxyz²δ, u)
+    var"α²β|S̄|S̄₁₁" = p.α^2 * p.β * S_abs * ∂xᶜᶜᶜ(i, j, k, grid, ℱxyz²ᵟ, u)
     #m₂₂² = p.α^2 * p.β * S_abs * ∂yᶜᶜᶜ(i, j, k, grid, ℱxyz²δ, v)
     #m₃₃² = p.α^2 * p.β * S_abs * ∂zᶜᶜᶜ(i, j, k, grid, ℱxyz²δ, w)
 
@@ -57,18 +71,8 @@ function MᵢⱼMᵢⱼ_ccc(i, j, k, grid, u, v, w, p)
 end
 
 
-
-
-simulation = Simulation(model, Δt=0.2, stop_time=50)
-
-wizard = TimeStepWizard(cfl=0.7, max_change=1.1, max_Δt=0.5)
-simulation.callbacks[:wizard] = Callback(wizard, IterationInterval(10))
-add_callback!(simulation, BasicTimeMessenger(), IterationInterval(100))
-
 u, v, w = model.velocities
 
-S = KernelFunctionOperation{Center, Center, Center}(strain_rate_tensor_modulus_ccc, model.grid, u, v, w)
-@show compute!(Field(S))
 
 ω = ∂x(v) - ∂y(u)
 ω̃ = KernelFunctionOperation{Face, Face, Center}(ℱxy²ᵟ, grid, ω)
@@ -76,14 +80,30 @@ S = KernelFunctionOperation{Center, Center, Center}(strain_rate_tensor_modulus_c
 ū = KernelFunctionOperation{Face, Center, Center}(ℱxyz²ᵟ, grid, u)
 v̄ = KernelFunctionOperation{Center, Face, Center}(ℱxyz²ᵟ, grid, v)
 w̄ = KernelFunctionOperation{Center, Center, Face}(ℱxyz²ᵟ, grid, w)
-S̄ = KernelFunctionOperation{Center, Center, Center}(strain_rate_tensor_modulus_ccc, model.grid, ū, v̄, w̄)
-@show compute!(Field(S))
 
+S = KernelFunctionOperation{Center, Center, Center}(strain_rate_tensor_modulus_ccc, model.grid, u, v, w)
+@show compute!(Field(S))
+S̄ = KernelFunctionOperation{Center, Center, Center}(filtered_strain_rate_tensor_modulus_ccc, model.grid, u, v, w)
+@show compute!(Field(S̄))
+
+params = (α = 2, β = 1)
+Mij² = KernelFunctionOperation{Center, Center, Center}(MᵢⱼMᵢⱼ_ccc, model.grid, u, v, w, params)
+@show compute!(Field(Mij²))
+
+
+pause
+#+++ Set up simulation
+simulation = Simulation(model, Δt=0.2, stop_time=50)
+
+wizard = TimeStepWizard(cfl=0.7, max_change=1.1, max_Δt=0.5)
+simulation.callbacks[:wizard] = Callback(wizard, IterationInterval(10))
+add_callback!(simulation, BasicTimeMessenger(), IterationInterval(100))
+#---
 
 # We pass these operations to an output writer below to calculate and output them during the simulation.
 filename = "two_dimensional_turbulence"
 
-simulation.output_writers[:fields] = JLD2OutputWriter(model, (; ω, ω̃),
+simulation.output_writers[:fields] = JLD2OutputWriter(model, (; ω, ω̃, S, S̄),
                                                       schedule = TimeInterval(0.6),
                                                       filename = filename * ".jld2",
                                                       overwrite_existing = true)
@@ -111,21 +131,13 @@ set_theme!(Theme(fontsize = 24))
 
 fig = Figure(size = (800, 500))
 
-axis_kwargs = (xlabel = "x",
-               ylabel = "y",
-               limits = ((0, 2π), (0, 2π)),
-               aspect = AxisAspect(1))
+axis_kwargs = (xlabel = "x", ylabel = "y", limits = ((0, 2π), (0, 2π)), aspect = AxisAspect(1))
 
 ax_ω = Axis(fig[2, 1]; title = "Vorticity", axis_kwargs...)
 ax_s = Axis(fig[2, 2]; title = "Speed", axis_kwargs...)
-nothing #hide
 
-# We use Makie's `Observable` to animate the data. To dive into how `Observable`s work we
-# refer to [Makie.jl's Documentation](https://makie.juliaplots.org/stable/documentation/nodes/index.html).
 
 n = Observable(1)
-
-# Now let's plot the vorticity and speed.
 
 ω = @lift interior(ω_timeseries[$n], :, :, 1)
 ω̃ = @lift interior(ω̃_timeseries[$n], :, :, 1)
